c425be766785129bc73902f466149cc1bca57952
Kayo Werter <kayo.werter1@gmail.com>
2023-12-21 09:58:56 -0300

    atualizacoes templates

--- estoque/urls.py
+++ estoque/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from estoque.views import index
+from estoque.views import index, estoque, cliente, adicionar_estoque, novo_cliente
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='index'),
+    path('estoque/', estoque, name='estoque'),
+    path('cliente/', cliente, name='cliente'),
+    path('adicionar_estoque/', adicionar_estoque, name='adicionar_estoque'),
+    path('novo_cliente/', novo_cliente, name='novo_cliente'),
 ]
--- estoque/views.py
+++ estoque/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'estoque/index.html')
+
+def estoque(request):
+    return render(request, 'estoque/estoque.html')
+
+def cliente(request):
+    return render(request, 'estoque/cliente.html')
+
+def adicionar_estoque(request):
+    return render(request, 'estoque/adicionar_estoque.html')
+
+
+def novo_cliente(request):
+    return render (request, 'estoque/novo_cliente.html')
